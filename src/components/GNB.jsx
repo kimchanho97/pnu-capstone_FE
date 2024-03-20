d327f7c886733f3bbdd36b37799102310679a573
kimchanho97 <nh0903@pusan.ac.kr>
2024-03-20 23:08:35 +0900

    feat: Implement GNB

---
+++ src/components/GNB.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function GNB() {
+  return (
+    <div className=" border-b p-5 flex justify-between">
+      <Link className=" font-mono font-semibold" to={"/"}>
+        pnuCloud
+      </Link>
+      <div className=" text-sm flex gap-2 items-center font-medium">
+        <Link>로그인</Link>
+        <Link to={"/create"} className=" bg-black text-white p-1 px-2 rounded">
+          시작하기
+        </Link>
+      </div>
+    </div>
+  );
+}
