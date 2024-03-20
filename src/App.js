c6f3945925b0ee28bd6f358fb1d67fa4391db32e
kimchanho97 <nh0903@pusan.ac.kr>
2024-03-21 01:04:27 +0900

    feat: Implement StartPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import MainLayout from "./layouts/MainLayout";
 import StartPage from "./pages/StartPage";
+import LoginPage from "./pages/LoginPage";
+import AppPage from "./pages/AppPage";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/create" element={<StartPage />} />
         </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/app" element={<AppPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/GNB.jsx
+++ src/components/GNB.jsx
@@ -8,7 +8,7 @@ export default function GNB() {
         pnuCloud
       </Link>
       <div className=" text-sm flex gap-2 items-center font-medium">
-        <Link>로그인</Link>
+        <Link to={"/login"}>로그인</Link>
         <Link to={"/create"} className=" bg-black text-white p-1 px-2 rounded">
           시작하기
         </Link>
