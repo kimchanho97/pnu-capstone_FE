d3b6fd92b87d02db31749338f1bcd4795faaec88
kimchanho97 <nh0903@pusan.ac.kr>
2024-04-24 09:42:14 +0900

    feat: Implement StartPage

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { templateIcons } from "../utils/constant";
 
 export default function StartPage() {
-  return <div>StartPage</div>;
+  return (
+    <div className=" pt-20 px-5 bg-zinc-50">
+      <div className=" flex flex-col gap-3">
+        <h1 className=" font-semibold text-[40px]">템플릿으로 시작하기</h1>
+        <h2>예제 템플릿으로 지금 바로 시작해보세요.</h2>
+        <div className=" grid grid-cols-3 gap-5 mt-14">
+          {templateIcons.map(({ Icon, title, subtitle }) => (
+            <div className=" flex flex-col justify-between border rounded-xl shadow-md p-5 h-[200px]">
+              <div className=" flex gap-2">
+                <Icon className=" w-10 h-10" />
+                <div className=" flex flex-col text-sm">
+                  <span className=" font-semibold">{title}</span>
+                  <span>{subtitle}</span>
+                </div>
+              </div>
+              <div className=" flex justify-end">
+                <button className=" bg-zinc-200 rounded-md p-2 w-20 text-sm ">
+                  배포하기
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className=" h-[300px]" />
+    </div>
+  );
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,39 @@
+import { ReactComponent as AngularIcon } from "../assets/angular.svg";
+import { ReactComponent as DjangoIcon } from "../assets/django.svg";
+import { ReactComponent as DockerIcon } from "../assets/docker.svg";
+import { ReactComponent as FastapiIcon } from "../assets/fastapi.svg";
+import { ReactComponent as FlaskIcon } from "../assets/flask.svg";
+import { ReactComponent as HtmlIcon } from "../assets/html.svg";
+import { ReactComponent as KotlinIcon } from "../assets/kotlin.svg";
+import { ReactComponent as MongodbIcon } from "../assets/mongodb.svg";
+import { ReactComponent as MysqlIcon } from "../assets/mysql.svg";
+import { ReactComponent as NestjsIcon } from "../assets/nestjs.svg";
+import { ReactComponent as NextjsIcon } from "../assets/nextjs.svg";
+import { ReactComponent as NginxIcon } from "../assets/nginx.svg";
+import { ReactComponent as NodejsIcon } from "../assets/nodejs.svg";
+import { ReactComponent as PostgresqlIcon } from "../assets/postgresql.svg";
+import { ReactComponent as ReactIcon } from "../assets/react.svg";
+import { ReactComponent as SpringIcon } from "../assets/spring.svg";
+import { ReactComponent as SvelteIcon } from "../assets/svelte.svg";
+import { ReactComponent as VueIcon } from "../assets/vue.svg";
+
+export const templateIcons = [
+  { Icon: ReactIcon, subtitle: "web", title: "React" },
+  { Icon: VueIcon, subtitle: "web", title: "Vue" },
+  { Icon: AngularIcon, subtitle: "web", title: "Angular" },
+  { Icon: HtmlIcon, subtitle: "web", title: "HTML" },
+  { Icon: SvelteIcon, subtitle: "web", title: "Svelte" },
+  { Icon: NextjsIcon, subtitle: "server", title: "Next.js" },
+  { Icon: NestjsIcon, subtitle: "server", title: "Nest.js" },
+  { Icon: NodejsIcon, subtitle: "server", title: "Node.js" },
+  { Icon: KotlinIcon, subtitle: "server", title: "Kotlin" },
+  { Icon: DjangoIcon, subtitle: "server", title: "Django" },
+  { Icon: FlaskIcon, subtitle: "server", title: "Flask" },
+  { Icon: FastapiIcon, subtitle: "server", title: "FastAPI" },
+  { Icon: SpringIcon, subtitle: "server", title: "Spring" },
+  { Icon: NginxIcon, subtitle: "server", title: "Nginx" },
+  { Icon: DockerIcon, subtitle: "docker", title: "Docker" },
+  { Icon: MysqlIcon, subtitle: "database", title: "MySQL" },
+  { Icon: MongodbIcon, subtitle: "database", title: "MongoDB" },
+  { Icon: PostgresqlIcon, subtitle: "database", title: "PostgreSQL" },
+];
