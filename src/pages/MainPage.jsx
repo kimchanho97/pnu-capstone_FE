9698f7b618ae182720376f921b4bd05829086dd7
kimchanho97 <nh0903@pusan.ac.kr>
2024-04-24 09:42:47 +0900

    design: Change main bg color

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,25 +5,26 @@ import IconGroup from "../components/IconGroup";
 export default function MainPage() {
   return (
     <div
-      className=" w-full flex p-40 items-center flex-col tracking-tight text-3xl font-semibold gap-10 rounded-[50px]"
+      className=" w-full flex p-40 items-center flex-col tracking-tight text-3xl font-semibold gap-10"
       style={{
         backgroundImage: `
-          linear-gradient(to right, 
-          rgba(191, 219, 254, 0.1),
-          rgba(250, 172, 168, 0.1), 
-          rgba(255, 255, 189, 0.1))
+            linear-gradient(127deg, rgba(255,0,0,.06), rgba(255,0,0,0) 70.71%),
+            linear-gradient(336deg, rgba(0,255,0,.06), rgba(0,255,0,0) 70.71%),
+            linear-gradient(217deg, rgba(0,0,255,.06), rgba(0,0,255,0) 70.71%)
         `,
         height: "calc(100vh - 100px)",
       }}
     >
       <div className=" flex flex-col items-center">
-        <p>복잡한 클라우드 설정 없이</p>
-        <p>10분 만에 서비스를 배포하고 운영하세요</p>
+        <h1>클라우드 배포의 새로운 경험</h1>
       </div>
-      <p className=" text-base">어떤 언어든 프레임웍이든 코드만 준비하세요.</p>
+      <div className=" text-base font-normal flex flex-col items-center">
+        <h2>배포부터 운영까지,</h2>
+        <h2>모든 단계를 간소화하여 개발자의 작업 효율을 최적화합니다</h2>
+      </div>
       <Link
         to={"/create"}
-        className=" bg-black text-white p-2 px-4 rounded text-sm"
+        className=" bg-black text-white p-2 px-6 rounded-lg text-sm"
       >
         템플릿으로 시작하기
       </Link>
