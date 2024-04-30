9087aab96d7f61261d8bcb13c54644ac988a0da6
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-01 08:48:40 +0900

    feat: Store accessToken in localstorage

--- src/apis/user.js
+++ src/apis/user.js
@@ -2,5 +2,6 @@ import { instance } from ".";
 
 export const githubLogin = async (code) => {
   const response = await instance.post("/user/login", { code });
+  localStorage.setItem("accessToken", response.headers.get("Authorization"));
   return response.data;
 };
