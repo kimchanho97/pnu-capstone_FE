cbd97c833ce6f37459371a6193c988507bc20753
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-01 08:14:05 +0900

    design: change GNB gap

--- src/components/GNB.jsx
+++ src/components/GNB.jsx
@@ -7,7 +7,7 @@ export default function GNB() {
       <Link className=" tracking-wider" to={"/"}>
         pitapat
       </Link>
-      <div className=" text-sm flex gap-2 items-center font-medium">
+      <div className=" text-sm flex gap-5 items-center font-medium">
         <Link to={"/login"}>로그인</Link>
         <Link to={"/create"} className=" bg-black text-white p-1 px-2 rounded">
           시작하기
