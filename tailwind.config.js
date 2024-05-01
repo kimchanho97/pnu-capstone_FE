5f7396b8c53db1dd2678fe2d1c7dc6bb1502df6c
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-01 09:50:32 +0900

    design: define media query width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        md: "1300px", // => @media (min-width: 1300px) { ... },
+        sm: "1000px", // => @media (min-width: 1000px) { ... },
+      },
+    },
   },
   plugins: [],
 };
