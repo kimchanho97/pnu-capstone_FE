1c27e402e2a5673f2052422c077643e502f9f437
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-09 23:45:34 +0900

    feat: Prevent to connect personal projectPage from other users

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import { useAtomValue } from "jotai";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import MainSection from "../components/project/MainSection";
 import SideSection from "../components/project/SideSection";
+import { userAtom } from "../store";
 
 export default function ProjectPage() {
+  const user = useAtomValue(userAtom);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const path = location.pathname.split("/")[2];
+    if (path !== user.login) {
+      navigate("/login");
+    }
+  }, [location.pathname, navigate, user.login]);
+
   return (
     <div className="flex">
       <SideSection />
