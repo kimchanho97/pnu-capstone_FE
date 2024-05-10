ad0b96456a923984a064f216f9fa6e36133f2d79
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-10 23:55:50 +0900

    feat: Implement get repo list of user from github

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,7 +1,24 @@
 import { instance } from ".";
+import axios from "axios";
 
 export const githubLogin = async (code) => {
   const response = await instance.post("/user/login", { code });
-  localStorage.setItem("accessToken", response.headers.get("Authorization"));
+  sessionStorage.setItem("accessToken", response.headers.get("Authorization"));
+  return response.data;
+};
+
+export const fetchRepos = async ({ login }) => {
+  const response = await axios.get(
+    `https://api.github.com/users/${login}/repos`,
+    {
+      headers: {
+        Authorization: sessionStorage.getItem("accessToken"),
+      },
+      params: {
+        sort: "updated",
+        type: "all",
+      },
+    },
+  );
   return response.data;
 };
