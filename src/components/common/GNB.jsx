480adfbd31b2074f257b5c81e0a640fb744ba80e
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-12 03:17:07 +0900

    feat: Implement conditional navigate in GNB

--- src/components/common/GNB.jsx
+++ src/components/common/GNB.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function GNB() {
+  const navigate = useNavigate();
+  const path = window.location.pathname;
+  const handleStartButton = () => {
+    if (path === "/create") {
+      navigate("/login");
+      return;
+    }
+    navigate("/create");
+  };
+
   return (
     <div className=" border-b p-5 flex justify-between h-[70px] px-16">
       <Link className=" tracking-wider" to={"/"}>
@@ -9,9 +19,12 @@ export default function GNB() {
       </Link>
       <div className=" text-sm flex gap-5 items-center font-medium">
         <Link to={"/login"}>로그인</Link>
-        <Link to={"/create"} className=" bg-black text-white p-1 px-2 rounded">
+        <button
+          className=" bg-black text-white p-1 px-2 rounded"
+          onClick={handleStartButton}
+        >
           시작하기
-        </Link>
+        </button>
       </div>
     </div>
   );
