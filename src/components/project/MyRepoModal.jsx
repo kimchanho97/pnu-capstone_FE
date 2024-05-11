6606518ab70c4dc9c234b617e89d0709b36227b7
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-12 05:13:57 +0900

    feat: Implement input port

--- src/components/project/MyRepoModal.jsx
+++ src/components/project/MyRepoModal.jsx
@@ -9,6 +9,7 @@ import useModal from "../../hooks/useModal";
 import { userAtom } from "../../store";
 import FrameworkList from "./FrameworkList";
 import RepoList from "./RepoList";
+import SecretVariableList from "./SecretVariableList";
 
 export default function MyRepoModal() {
   const { closeModal } = useModal();
@@ -16,11 +17,30 @@ export default function MyRepoModal() {
   const [selectedRepo, setSelectedRepo] = useState("");
   const [isFrameworkListOpen, setIsFrameworkListOpen] = useState(false);
   const [selectedFramework, setSelectedFramework] = useState("");
+  const [secretVariables, setSecretVariables] = useState([]);
+  const [port, setPort] = useState(null);
   const user = useAtomValue(userAtom);
   const { isLoading, data: repos } = useQuery(
     ["/repos", user.login], // 쿼리 키에 user.login을 추가하여 유저마다 캐시 관리
     () => fetchRepos({ login: user.login }), // 함수 참조 대신 익명 함수 사용하여 호출
   );
+  const isBackend = [
+    "nextjs",
+    "nestjs",
+    "nodejs",
+    "kotlin",
+    "django",
+    "flask",
+    "fastapi",
+    "spring",
+    "nginx",
+    "docker",
+  ].includes(selectedFramework);
+
+  const handlePortChange = (e) => {
+    const { value } = e.target;
+    setPort(value);
+  };
 
   return (
     <div className=" fixed top-0 left-0 w-screen h-screen bg-[rgba(31,41,55,0.2)] flex justify-center items-center z-10">
@@ -58,10 +78,10 @@ export default function MyRepoModal() {
             />
           )}
           {selectedRepo ? (
-            <div className=" mt-16 flex flex-col gap-10 mb-48">
+            <div className=" mt-16 flex flex-col gap-10 mb-20">
               <div>
-                <div className=" text-sm">프로젝트 이름</div>
-                <div className=" w-full h-[40px] border mt-1 pl-3 text-zinc-500 flex items-center bg-zinc-50">
+                <div className=" text-sm mb-1">프로젝트 이름</div>
+                <div className=" w-full h-[40px] border pl-3 text-zinc-500 flex items-center bg-zinc-50">
                   {selectedRepo}
                 </div>
                 <div className=" text-sm p-1 text-blue-500">
@@ -79,11 +99,27 @@ export default function MyRepoModal() {
                 />
               </div>
               <div>
-                <div className=" text-sm">Secret Variables</div>
-                <div className=" w-full h-[40px] border mt-1 pl-3 text-zinc-500 flex items-center bg-zinc-50">
-                  {selectedRepo}
-                </div>
+                <div className=" text-sm mb-1">Secret Variables</div>
+                <SecretVariableList
+                  secretVariables={secretVariables}
+                  setSecretVariables={setSecretVariables}
+                />
               </div>
+              {isBackend && (
+                <div>
+                  <div className=" text-sm mb-1">Port</div>
+                  <input
+                    type="text"
+                    className="w-full border h-[40px] p-4"
+                    placeholder="Port"
+                    value={port}
+                    onChange={handlePortChange}
+                  />
+                </div>
+              )}
+              <button className=" w-full h-[40px] bg-blue-500 text-white rounded-lg mt-10">
+                배포하기
+              </button>
             </div>
           ) : (
             <div className=" mt-16 text-center text-zinc-500">
