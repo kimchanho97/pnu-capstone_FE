6ca1e96d38726be65d60b0f486a78a2a36041e38
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-12 09:25:23 +0900

    feat: Modify fetchRepos to show private repo

--- src/apis/user.js
+++ src/apis/user.js
@@ -3,22 +3,22 @@ import axios from "axios";
 
 export const githubLogin = async (code) => {
   const response = await instance.post("/user/login", { code });
-  sessionStorage.setItem("accessToken", response.headers.get("Authorization"));
+  const accessToken = response.headers.get("Authorization");
+  const token = accessToken.replace("Bearer ", "");
+  sessionStorage.setItem("accessToken", token);
   return response.data;
 };
 
 export const fetchRepos = async ({ login }) => {
-  const response = await axios.get(
-    `https://api.github.com/users/${login}/repos`,
-    {
-      headers: {
-        Authorization: sessionStorage.getItem("accessToken"),
-      },
-      params: {
-        sort: "updated",
-        type: "all",
-      },
+  const token = sessionStorage.getItem("accessToken");
+  const response = await axios.get(`https://api.github.com/user/repos`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/vnd.github.v3+json",
     },
-  );
-  return response.data;
+    params: {
+      sort: "updated",
+    },
+  });
+  return response.data.filter((repo) => repo.owner.login === login);
 };
