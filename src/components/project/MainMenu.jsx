fcb81a7486ae69092c68ef1afad4bcff2d3f3d0f
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-12 09:21:35 +0900

    feat: Clear all state when logout

--- src/components/project/MainMenu.jsx
+++ src/components/project/MainMenu.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { IoMdExit } from "react-icons/io";
-import { Link } from "react-router-dom";
 import { FiHome } from "react-icons/fi";
+import { IoMdExit } from "react-icons/io";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function MainMenu() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    sessionStorage.clear();
+    navigate("/login");
+  };
   return (
     <div className=" w-full h-[50px] flex justify-end items-center pr-5">
       <div className=" flex gap-5">
@@ -11,10 +16,10 @@ export default function MainMenu() {
           <FiHome />
           <span>Home</span>
         </Link>
-        <Link className=" flex items-center" to={"/login"}>
+        <button className=" flex items-center" onClick={handleLogout}>
           <IoMdExit />
           <span>LogOut</span>
-        </Link>
+        </button>
       </div>
     </div>
   );
