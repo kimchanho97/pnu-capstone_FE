ca9baff79d7dbc42e785340f4821c85a9a9b47c8
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-17 21:48:37 +0900

    feat: Implement ProjectDetail

--- src/components/project/ProjectDetail.jsx
+++ src/components/project/ProjectDetail.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { IoChevronBackOutline } from "react-icons/io5";
+import ConnectionInfo from "./ConnectionInfo";
+import DeploymentHistory from "./DeploymentHistory";
+import ProjectDetailMenu from "./ProjectDetailMenu";
+import ProjectItem from "./ProjectItem";
+import ProjectSetting from "./ProjectSetting";
 
 const projectDetail = {
   deployments: [
@@ -27,6 +33,37 @@ const projectDetail = {
   ],
 };
 
-export default function ProjectDetail() {
-  return <div>ProjectDetail</div>;
+export default function ProjectDetail({ project, setSelectedProject }) {
+  const [selectedDetailOption, setSelectedDetailOption] = useState("배포 내역");
+
+  useEffect(() => {
+    console.log(project);
+  }, []);
+
+  return (
+    <div className=" flex flex-col w-full">
+      <button
+        className=" flex items-center gap-2 mb-3 text-zinc-500"
+        onClick={() => setSelectedProject(null)}
+      >
+        <IoChevronBackOutline size={20} />
+        <span>{project.name}</span>
+      </button>
+      <ProjectItem
+        project={project}
+        className={"border p-5 h-[150px] w-full bg-white"}
+      />
+      <ProjectDetailMenu
+        selectedDetailOption={selectedDetailOption}
+        setSelectedDetailOption={setSelectedDetailOption}
+      />
+      {selectedDetailOption === "배포 내역" ? (
+        <DeploymentHistory deployments={projectDetail.deployments} />
+      ) : selectedDetailOption === "연결 정보" ? (
+        <ConnectionInfo />
+      ) : (
+        <ProjectSetting secrets={projectDetail.secrets} />
+      )}
+    </div>
+  );
 }
